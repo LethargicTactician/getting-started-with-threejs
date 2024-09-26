3bf37a71f3920297a91cfb3428b4573e2834a5ad
LethargicTactician <zhuran28@gmail.com>
2024-09-26 15:23:38 -0700

    getting started with threejs

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Robot Bobby was here');
+import * as THREE from 'three';
+
+//full tutorial follow here: https://youtu.be/XPhAR1YdD6o?si=7fe_vl_35teHjf2a
+
+const w = window;
+const render = new THREE.WebGLRenderer({antialias: true});
